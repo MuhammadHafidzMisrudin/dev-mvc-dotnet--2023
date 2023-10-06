ae3f2bb8c96a37e8319c308f7613e93dfd52551f
Muhammad Hafidz <halfpheez92@gmail.com>
2023-10-06 22:18:16 +0800

    added dev mvc project.

---
+++ DevMVCApp/DevMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Products() {
+            return View("OurCompanyProducts");
+        }
+
+        public ActionResult Contact() { 
+            return View("Contact"); 
+        }
+
+        public ActionResult GetEmpName(int EmpId) {
+            var employees = new[] {
+                new { EmpId = 1, EmpName = "Scott", Salary = 8000 },
+                new { EmpId = 2, EmpName = "Smith", Salary = 2540 },
+                new { EmpId = 3, EmpName = "Allen", Salary = 9400 }
+            };
+
+            string matchName = null;
+            foreach (var item in employees) {
+                if (EmpId == item.EmpId) {
+                    matchName = item.EmpName;
+                }
+            }
+
+            //return View();
+            return Content(matchName, "text/plain");
+        }
+    }
+}
---
+++ DevMVCApp/DevMVCApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevMVCApp.MvcApplication" Language="C#" %>
