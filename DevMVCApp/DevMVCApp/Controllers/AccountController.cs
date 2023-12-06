ff4ac12e2a19cc300b9146a7e63d04313d367339
Muhammad Hafidz <halfpheez92@gmail.com>
2023-12-07 02:04:10 +0800

    added new controllers, account and admin for action result for redirect to route.

---
+++ DevMVCApp/DevMVCApp/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevMVCApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account's Login action method.
+        // ~/account/login?username=admin&password=manager
+        public ActionResult Login(string Username, string Password) {
+            if(Username == "admin" && Password == "manager") {
+                return RedirectToAction("Dashboard", "Admin");
+            } else {
+                return RedirectToAction("InvalidLogin", "Account");
+            }
+        }
+
+        // GET: InvalidLogin action method.
+        public ActionResult InvalidLogin() {
+            return View();
+        }
+    }
+}
---
+++ DevMVCApp/DevMVCApp/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevMVCApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin's Dashboard action method.
+        public ActionResult Dashboard() {
+            return View();
+        }
+    }
+}
